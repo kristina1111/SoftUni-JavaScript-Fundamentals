ba48b4eb680e46452be669bcf5d0c3835926ba5c
kristina1111 <kristina.k.kovacheva@gmail.com>
2017-05-22 21:48:40 +0300

    added solutions to some exercises

---
+++ E02.sumAndVat.js
@@ -0,0 +1,10 @@
+function solver(arr) {
+    let sum = arr.reduce(
+        (accumulate, current) => accumulate + current
+    );
+    console.log(`sum = ${sum.toFixed(2)}`);
+    console.log(`VAT = ${(sum * 0.2).toFixed(2)}`);
+    console.log(`total = ${(sum * 1.2).toFixed(2)}`);
+}
+
+solver([1,2,3])
